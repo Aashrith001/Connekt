77cb6afc494776421eb5e134e970cab78f5f210e
Aashrith001 <v.aashrith22@gmail.com>
2021-05-07 18:45:45 +0000

    minor changes

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ function isLoggedIn(req, res, next) {
     res.redirect('/');
 };
 
-app.listen(process.env.PORT || 3001, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Connekt server is running");
 });
 
